5df1545ff84f388c2b00364616cfecc6abe9d627
Rachel Isaac <86608086+Chelsofia@users.noreply.github.com>
2022-11-01 14:19:34 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="div">
+          <div className="div0">
+            <div className="pic"></div>
+            <div className="share" title="share link">
+              <a href="/"></a>
+            </div>
+            <div className="twit">
+              <p id="twitter">iamchelsofia</p>
+              <p id="slack" hidden>Rachel Asse</p>
+            </div>
+          </div>
+          <div></div>
+          <button className="div1">
+            <a href="https://twitter.com/iamchelsofia/" id="twitter">
+              Twitter Link
+            </a>
+          </button>
+          <button type="button" className="div1" id="btn_zuri">
+            <a href="https://training.zuri.team/">Zuri Team</a>
+          </button>
+          <button className="div1" id="books">
+            <a href="http://books.zuri.team ">Zuri Books</a>
+          </button>
+          <button className="div1" id="book_python">
+            <a href="https://books.zuri.team">Python Books</a>
+          </button>
+          <button className="div1" id="book_design">
+            <a href="https://background.zuri.team">
+              Background Check for Coders
+            </a>
+          </button>
+          <button className="div1">
+            <a href="https://books.zuri.team/design-rules">Design Books</a>
+          </button>
+          <div className="slack">
+            <div className="github">
+              <a href="https://github.com/Chelsofia"></a>
+            </div>
+            <div className="slackk"></div>
+          </div>
+          <footer>
+            <div className="hr">
+              <hr />
+            </div>
+            <div className="all">
+              <div className="zuri"></div>
+              <div className="text">HNG Internship 9 Frontend Task</div>
+              <div className="i4g"></div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
