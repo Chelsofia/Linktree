11eebb6f8b1ac33f1fb6ee44101143a579b84339
Rachel Isaac <86608086+Chelsofia@users.noreply.github.com>
2022-11-01 16:08:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
           <div className="div0">
             <div className="pic"></div>
             <div className="share" title="share link">
-              <a href="https://twitter.com"><br/></a>
+              <a href="https://0.com"><br/></a>
             </div>
             <div className="twit">
               <p id="twitter">iamchelsofia</p>
